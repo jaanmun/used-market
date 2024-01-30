941dd5605fe3bdd4cfc17424291b9bda8a52a092
JaanMun <yjm7575@naver.com>
2024-01-30 13:14:32 +0900

    [Feat] Implementing membership sign-up function
    api 통신을 위해 axios 패키지 설치.
    비밀번호 암호화를 위해 bcryptjs 패키지 설치하고 type 정의를 위해 @types/bcryptjs 타입 모듈 설치.
    하나의 prisma client 객체를 여러 곳에서 재활용하여 재사용성을 높이기 위해 prismadb.ts 파일 생성.
    로그인 및 회원가입 커스텀 페이지 추가를 위해 loginPage와 RegisterPage 생성.

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,19 +2,22 @@
 
 import Button from '@/components/Button';
 import Input from '@/components/Input';
-import { signIn } from 'next-auth/react';
+import axios from 'axios';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
+      name: '',
       email: '',
       password: '',
     },
@@ -23,8 +26,9 @@ const LoginPage = () => {
   const onSubmit: SubmitHandler<FieldValues> = async body => {
     setIsLoading(true);
     try {
-      const data = signIn('credentials', body);
+      const { data } = await axios.post('/api/register', body);
       console.log({ data });
+      router.push('/auth/login');
     } catch (error) {
       console.log(error);
     } finally {
@@ -35,16 +39,14 @@ const LoginPage = () => {
   return (
     <section className="grid h-[calc(100vh_-_56px)] place-items-center">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center gap-4 min-w-[350px]">
-        <h1 className="text-2xl">Login</h1>
+        <h1 className="text-2xl">Register</h1>
         <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
+        <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
         <Input id="password" label="Password" type="password" disabled={isLoading} register={register} errors={errors} required />
         <Button label="Register" />
         <div className="text-center">
           <p className="text-gray-400">
-            Not a member?{' '}
-            <Link href="/auth/register" className="text-black hover:underline">
-              Register
-            </Link>
+            Already a member? <Link href="/auth/login" className="text-black hover:underline">Login</Link>
           </p>
         </div>
       </form>
@@ -52,4 +54,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -20,12 +20,14 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, forma
         id={id}
         disabled={disabled}
         {...register(id, { required })}
+        placeholder=""
         type={type}
         className={`
                w-full 
                p-4 
                pt-6 
                font-light 
+               border-2
                bg-white 
                rounded-md 
                outline-none 
@@ -33,7 +35,7 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, forma
                disabled: opacity-70 
                disabled: cursor-not-allowed 
                ${formatPrice ? 'pl-9' : 'pl-4'} 
-               ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}}
+               ${errors[id] ? 'border-rose-500' : 'border-neutral-300'}
                ${errors[id] ? 'focus: border-rose-500' : 'focus: border-black'}
           `}
       />
@@ -54,7 +56,9 @@ const Input: React.FC<InputProps> = ({ id, label, type = 'text', disabled, forma
           peer-focus: -translate-y-4
           ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
       `}
-      ></label>
+      >
+        {label}
+      </label>
     </div>
   );
 };
